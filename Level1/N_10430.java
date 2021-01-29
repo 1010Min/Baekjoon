26bb3f0229a152ed1e764e6f572c33d894358bb3
1010Min <1010ally@naver.com>
2021-01-29 10:48:17 +0900

    Level1

---
+++ Level1/N_10430.java
@@ -0,0 +1,18 @@
+//첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+import java.util.Scanner;
+
+public class N_10430 {
+	public static void main(String[] args) {
+		int a,b,c;
+		
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+		
+		System.out.println((a+b)%c);
+		System.out.println(((a%c)+(b%c))%c);
+		System.out.println((a*b)%c);
+		System.out.println(((a%c)*(b%c))%c);
+	}
+}
---
+++ Level1/N_10998.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class N_10998 {
+
+	public static void main(String[] args) {
+		int a, b;
+		
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		System.out.println(a*b);
+	}
+
+}
