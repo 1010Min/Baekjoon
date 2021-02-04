59328235e146116fd46121c192a4bc2947ad8b63
1010Min <1010ally@naver.com>
2021-02-04 18:55:46 +0900

    아스키 코드

---
+++ Level7/N_11654.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class N_11654 {
+
+	public static void main(String[] args) {
+		int input;
+		
+		Scanner sc = new Scanner(System.in);
+		input = sc.next().charAt(0);
+		
+		System.out.println(input);
+	}
+
+}
