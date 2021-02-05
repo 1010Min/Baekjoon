dfa396245fc3e4390702bbbed9e12add77fab095
1010Min <1010ally@naver.com>
2021-02-05 20:23:39 +0900

    알파벳 찾기

---
+++ Level7/N_10809.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class N_10809 {
+
+	public static void main(String[] args) {
+		
+		 Scanner sc = new Scanner(System.in);
+         String input = sc.next();
+         
+         for (char c = 'a' ; c <= 'z' ; c++)
+        	 System.out.print(input.indexOf(c) + " ");
+         
+	}
+
+}
