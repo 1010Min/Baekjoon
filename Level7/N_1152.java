cc9f5c5c31e3fd82cfdaabcfc5ea83045d5e508f
1010Min <1010ally@naver.com>
2021-02-08 14:49:58 +0900

    단어의 개수

---
+++ Level7/N_1152.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class N_1152 {
+
+	public static void main(String[] args) {
+		int count = 0;
+		Scanner sc = new Scanner(System.in);
+		
+		String input = sc.nextLine();
+		
+		StringTokenizer st = new StringTokenizer(input, " ");
+		
+		System.out.println(st.countTokens()); // Token 개수 반환
+		
+	}
+
+}
