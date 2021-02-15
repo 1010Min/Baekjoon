8eaf7ad9396376fb9547e4a659b0bc3c296246e2
1010Min <1010ally@naver.com>
2021-02-15 12:13:09 +0900

    (수정) 달팽이는 올라가고 싶다

--- Level8/N_2869.java
+++ Level8/N_2869.java
@@ -19,25 +19,3 @@ public class N_2869 {
 		br.close();
 	}
 }
-
-// 이렇게 풀면 시간 초과! -> 정상에 올라간 후 미끄러지지 않는다는 조건 중요
-//public class N_2869 {
-//	public static void main(String[] args) throws IOException {
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		
-//		int A = Integer.parseInt(st.nextToken());
-//		int B = Integer.parseInt(st.nextToken());
-//		int V = Integer.parseInt(st.nextToken());
-//		int sum = A;
-//		int day = 1;
-//		
-//		while(V > sum) {
-//			sum += (A-B);
-//			day++;
-//		}
-//		
-//		System.out.println(day);
-//		br.close();
-//	}
-//}
