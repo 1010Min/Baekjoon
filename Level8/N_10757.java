a2c68fd9df3e2997b45b83ed5bd78e109f7f0705
1010Min <1010ally@naver.com>
2021-02-16 11:47:30 +0900

    (수정) 큰 수 A+B

--- Level8/N_10757.java
+++ Level8/N_10757.java
@@ -16,18 +16,3 @@ public class N_10757 {
 		br.close();
 	}
 }
-
-// 이렇게 하면 틀림!
-//public class N_10757 {
-//	public static void main(String[] args) throws IOException{
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		
-//		double A = Double.parseDouble(st.nextToken());
-//		double B = Double.parseDouble(st.nextToken());
-//		
-//		System.out.println(A+B);
-//		
-//		br.close();
-//	}
-//}
