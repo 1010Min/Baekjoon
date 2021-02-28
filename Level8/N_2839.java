7953c86651e9b995098afe8eea66708a0270ccf2
1010Min <1010ally@naver.com>
2021-02-28 10:26:52 +0900

    (수정) 설탕 배달

--- Level8/N_2839.java
+++ Level8/N_2839.java
@@ -1,42 +1,5 @@
 import java.io.*;
 
-// 방법 1
-//public class N_2839 {
-//	public static void main(String[] args) throws IOException{
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		
-//		int N = Integer.parseInt(br.readLine());
-//		
-//		if (N == 4 || N == 7) {
-//			System.out.println(-1);
-//		}
-//		else if (N % 5 == 0) {
-//			System.out.println(N / 5);
-//		}
-//		else if (N % 5 == 1 || N % 5 == 3) {
-//			System.out.println((N / 5) + 1);
-//		}
-//		else if (N % 5 == 2 || N % 5 == 4) {
-//			System.out.println((N / 5) + 2);
-//		}
-//		
-//	}
-//}
-//
-//// /는 몫이고 %는 나머지
-///*
-//	n		3	4	5	6	7	8	9	10	11	12	13	14	15	16	17	18	19	20	21	22	23	24	25
-//	개수		1	-1	1	2	-1	2	3	2	3	4	3	4	3	4	5	4	5	4	5	6	5	6	5
-//	n/5		0	0	1	1	1	1	1	2	2	2	2	2	3	3	3	3	3	4	4	4	4	4	5
-//	n%5		3	4	0	1	2	3	4	0	1	2	3	4	0	1	2	3	4	0	1	2	3	4	0
-//	
-//	5의 배수+3 -> 봉지 개수 최소
-//	5의 배수+1 -> 봉지 개수 1개 증가
-//	5의 배수-1(n=4일 때 제외) -> 봉지 개수 3에서 1씩 증가
-//	12+5의 배수 -> 봉지 1씩 증가
-//*/
-
-// 방법 2
 public class N_2839 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
